901b00e5e7262e83c4e6e56f661e5ad17587e2ac
SU5_HamirulHafizal <softwareu5@i-smart.com.sg>
2022-10-03 16:41:22 +0800

    update socket

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const path = require('path');
-const { createServer } = require('http');
+const express = require("express");
+const path = require("path");
+const { createServer } = require("http");
 
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
 const app = express();
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', function (ws) {
-  const id = setInterval(function () {
-    ws.send(JSON.stringify(process.memoryUsage()), function () {
-      //
-      // Ignore errors.
-      //
-    });
-  }, 100);
-  console.log('started client interval');
+// ESTABLISH CONNECTION WEBSOCKET
+wss.on("connection", (ws) => {
 
-  ws.on('close', function () {
-    console.log('stopping client interval');
-    clearInterval(id);
+  // SERVER RECEIVED FROM CLIENT
+  ws.on("message", (data, isBinary) => {
+    // const dataRec = JSON.stringify(data);
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+
+  // SERVER RECEIVED CLOSE BROWSER FROM CLIENT
+  ws.on("close", () => {
+    console.log("stopping client interval");
   });
 });
 
-server.listen(8080, function () {
-  console.log('Listening on http://127.0.0.1:8080');
+server.listen(8080, () => {
+  console.log("Listening on http://127.0.0.1:8080");
 });
